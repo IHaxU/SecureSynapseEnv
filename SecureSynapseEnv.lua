94d2c5aeb26346519a81a42420fe35b49f6233ce
IHaxU <30863082+IHaxU@users.noreply.github.com>
2023-03-01 22:28:51 +0100

    Update SecureSynapseEnv.lua

--- SecureSynapseEnv.lua
+++ SecureSynapseEnv.lua
@@ -5,24 +5,24 @@ local newcclosure = newcclosure
 local next = next
 local getinfo = debug.getinfo
 local is_synapse_function = is_synapse_function
-local realGenv = {}
+local gameEnv = {}
 for i, v in next, getgenv() do
-    realGenv[i] = v
+    gameEnv[i] = v
     getgenv()[i] = nil
 end
 local mt = getrawmetatable(getgenv())
 local robloxEnv = mt.__index -- mt.__index == getrenv() -> true
 mt.__index = newcclosure(function(self, idx)
     if checkcaller() and is_synapse_function(getinfo(3).func) then -- 3 with newcclosure, 2 without.
-        if realGenv[idx] ~= nil then
-            return realGenv[idx]
+        if gameEnv[idx] ~= nil then
+            return gameEnv[idx]
         end
     end
     return robloxEnv[idx]
 end)
 mt.__newindex = newcclosure(function(self, idx, val)
     if checkcaller() and is_synapse_function(getinfo(3).func) then -- 3 with newcclosure, 2 without.
-        realGenv[idx] = val
+        gameEnv[idx] = val
     else
         robloxEnv[idx] = val
     end
