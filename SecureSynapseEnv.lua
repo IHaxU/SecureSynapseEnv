0155413e62b6908378660811df249a691a618b03
IHaxU <30863082+IHaxU@users.noreply.github.com>
2023-03-04 13:07:05 +0100

    Variable renaming

--- SecureSynapseEnv.lua
+++ SecureSynapseEnv.lua
@@ -5,24 +5,24 @@ local newcclosure = newcclosure
 local next = next
 local getinfo = debug.getinfo
 local is_synapse_function = is_synapse_function
-local gameEnv = {}
+local globalEnv = {}
 for i, v in next, getgenv() do
-    gameEnv[i] = v
+    globalEnv[i] = v
     getgenv()[i] = nil
 end
 local mt = getrawmetatable(getgenv())
 local robloxEnv = mt.__index -- mt.__index == getrenv() -> true
 mt.__index = newcclosure(function(self, idx)
     if is_synapse_function(getinfo(3).func) then -- 3 with newcclosure, 2 without.
-        if gameEnv[idx] ~= nil then
-            return gameEnv[idx]
+        if globalEnv[idx] ~= nil then
+            return globalEnv[idx]
         end
     end
     return robloxEnv[idx]
 end)
 mt.__newindex = newcclosure(function(self, idx, val)
     if is_synapse_function(getinfo(3).func) then -- 3 with newcclosure, 2 without.
-        gameEnv[idx] = val
+        globalEnv[idx] = val
     else
         robloxEnv[idx] = val
     end
